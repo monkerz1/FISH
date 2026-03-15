f951943102e9928279c15ba49aa25980c25a9b48
monkerz1 <killerpings@gmail.com>
2026-03-15 23:04:07 +0000

    Fix city pages: add 2-letter state code support to city page STATE_ABBR lookup

--- src/app/[state]/[city]/page.tsx
+++ src/app/[state]/[city]/page.tsx
@@ -15,7 +15,15 @@ const STATE_ABBR: Record<string, string> = {
   'new-york':'NY','north-carolina':'NC','north-dakota':'ND','ohio':'OH','oklahoma':'OK',
   'oregon':'OR','pennsylvania':'PA','rhode-island':'RI','south-carolina':'SC',
   'south-dakota':'SD','tennessee':'TN','texas':'TX','utah':'UT','vermont':'VT',
-  'virginia':'VA','washington':'WA','west-virginia':'WV','wisconsin':'WI','wyoming':'WY'
+  'virginia':'VA','washington':'WA','west-virginia':'WV','wisconsin':'WI','wyoming':'WY',
+  // 2-letter codes
+  'al':'AL','ak':'AK','az':'AZ','ar':'AR','ca':'CA','co':'CO','ct':'CT','de':'DE',
+  'fl':'FL','ga':'GA','hi':'HI','id':'ID','il':'IL','in':'IN','ia':'IA','ks':'KS',
+  'ky':'KY','la':'LA','me':'ME','md':'MD','ma':'MA','mi':'MI','mn':'MN','ms':'MS',
+  'mo':'MO','mt':'MT','ne':'NE','nv':'NV','nh':'NH','nj':'NJ','nm':'NM','ny':'NY',
+  'nc':'NC','nd':'ND','oh':'OH','ok':'OK','or':'OR','pa':'PA','ri':'RI','sc':'SC',
+  'sd':'SD','tn':'TN','tx':'TX','ut':'UT','vt':'VT','va':'VA','wa':'WA','wv':'WV',
+  'wi':'WI','wy':'WY'
 }
 
 interface CityPageProps {
